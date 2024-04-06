8d779f16c3acd57976aed4dc2f9f3f31a7de66fd
Hemanshi Prajapati <hemanshiprajapati8@gmail.com>
2024-04-06 19:40:07 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/RegistrationForm")
+  .then(() => console.log("Connected to Database"))
+  .catch((error) => console.log("Error in Connecting to Database:", error));
+
+  const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    confirm_password: String,
+    email:String
+  });
+
+const User = mongoose.model('User', UserSchema);
+
+app.post("/signup", async (req, res) => {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).send("Account already exists");
+    }
+    addEventListener
+    const user = new User(req.body);
+    await user.save();
+    console.log("Record Inserted Successfully");
+    res.redirect('success.html');
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email: email, password: password });
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+  console.log("Login Successful");
+  res.redirect(`/login_successful.html?email=${email}`);
+});
+
+
+
+app.get("/", (req, res) => {
+  res.redirect('index.html');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
