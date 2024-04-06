ba83fd3ad56179214580da6ae5cf1508269a2ba6
Hemanshi Prajapati <hemanshiprajapati8@gmail.com>
2024-04-06 20:31:18 +0530

    sec commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://localhost:27017/RegistrationForm")
+mongoose.connect('mongodb://localhost:27017/RegistrationForm')
   .then(() => console.log("Connected to Database"))
   .catch((error) => console.log("Error in Connecting to Database:", error));
 
@@ -20,19 +20,18 @@ mongoose.connect("mongodb://localhost:27017/RegistrationForm")
 
 const User = mongoose.model('User', UserSchema);
 
-app.post("/signup", async (req, res) => {
+app.post("/success", async (req, res) => {
     const existingUser = await User.findOne({ email: req.body.email });
     if (existingUser) {
       return res.status(400).send("Account already exists");
     }
-    addEventListener
     const user = new User(req.body);
     await user.save();
     console.log("Record Inserted Successfully");
     res.redirect('success.html');
 });
 
-app.post("/login", async (req, res) => {
+app.post("/login_successfull", async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email: email, password: password });
   if (!user) {
@@ -48,7 +47,7 @@ app.get("/", (req, res) => {
   res.redirect('index.html');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
